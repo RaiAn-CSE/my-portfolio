4f70bd71a14226d9efa44e55d1bbd0e70b123063
RaiAn-CSE <68633815+RaiAn-CSE@users.noreply.github.com>
2022-12-11 12:36:13 +0600

    footer added

--- src/Pages/ContactMe/ContactMe.js
+++ src/Pages/ContactMe/ContactMe.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const ContactMe = () => {
+    const formInfo = useRef();
+
+    const sendEmailInfo = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_qozfn3i', 'template_cu9t0ri', formInfo.current, 'bRta5-UTQGwt5suqH')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset();
+    };
     return (
-        <div>
-            <h2>this is contact Me</h2>
-        </div>
+        <form className='grid gap-3 justify-items-center' ref={formInfo} onSubmit={sendEmailInfo}>
+            <input type="text" name="user_name" placeholder="Type Name" className="input input-bordered input-sm w-full max-w-xs" />
+
+            <input type="email" name="user_email" placeholder="Type Email" className="input input-bordered input-md w-full max-w-xs" />
+
+            <input type="text" name="message" placeholder="Type Message" className="input input-bordered input-lg w-full max-w-xs" />
+            <button class="btn btn-outline btn-success h-6 w-20" type="submit" value="Send">Send</button>
+        </form>
     );
 };
 
--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -4,14 +4,14 @@ import { Typewriter } from 'react-simple-typewriter'
 
 const Banner = () => {
     return (
-        <div className='flex flex-col-reverse lg:flex-row lg:flex basis-2/4 my-8 mx-8'>
-            <div className='w-full lg:w-1/2 flex flex-col justify-center'>
+        <div className='flex items-center flex-col-reverse lg:flex-row my-8 mx-8'>
+            <div className='w-full items-center lg:w-1/2 flex flex-col justify-center'>
                 <div className="title">
                     <h1 className=" text-3xl leading-relaxed uppercase font-extrabold dark:text-white">Welcome to My Portfolio</h1>
                     <h1 className=" text-xl font-mono leading-relaxed uppercase font-extrabold text-orange-400  dark:text-white">Details more about me</h1>
                 </div>
                 <div>
-                    <p className='my-4 text-gray-500 dark:text-gray-400'> Hi This is Robiul Hasan Raian
+                    <p className='my-4 text-gray-500 dark:text-gray-400'> Hi, This is Robiul Hasan Raian
                         <br />
                         <span style={{ color: 'white', fontWeight: 'bold' }}>
                             {/* Style will be inherited from the parent element */}
@@ -28,11 +28,11 @@ const Banner = () => {
 
                 </div>
                 <div>
-                    <a href='https://drive.google.com/file/d/1e0lS7yLylI5o2mro_sG0j5rqIVCDu177/view?usp=sharing' target="_blank" rel="noopener noreferrer"><button type="button" class="text-white bg-orange-400 hover:bg-orange-500 focus:outline-none focus:ring-4 focus:ring-orange-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-orange-900">Resume</button></a>
+                    <a href='https://drive.google.com/file/d/1e0lS7yLylI5o2mro_sG0j5rqIVCDu177/view?usp=sharing' target="_blank" rel="noopener noreferrer"><button type="button" className="text-white bg-orange-400 hover:bg-orange-500 focus:outline-none focus:ring-4 focus:ring-orange-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:focus:ring-orange-900">Resume</button></a>
                 </div>
             </div>
-            <div className=" w-full lg:w-1/2">
-                <img className='w-full h-full rounded-full' src={img1} alt="banner" />
+            <div className="w-full lg:w-1/2">
+                <img className='w-5/6 h-5/6 rounded-full' src={img1} alt="banner" />
             </div>
         </div>
     );
